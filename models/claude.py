f2f7ca4fabd1e95019930aa553a00a9bb6d89d13
Andrei Paleyes <apalees@gmail.com>
2025-01-13 23:23:51 +0000

    Get started with Claude

---
+++ models/claude.py
@@ -0,0 +1,46 @@
+# some docs are here: https://github.com/anthropics/anthropic-sdk-python
+
+import os
+from anthropic import Anthropic
+
+from .base import BaseModel
+
+class Claude(BaseModel):
+    def __init__(self):
+        super().__init__("Claude")
+        self.client = Anthropic(
+            api_key=os.environ.get("ANTHROPIC_API_KEY"),
+        )
+
+        # possible values for temperature range from 0 to 1
+        # default is 1
+        # we use 0 to minimise variation
+        self.temperature = 0.0
+
+    def __call__(self, prompt):
+        message = self.client.messages.create(
+            # max declared for 3 Haiku model, see https://docs.anthropic.com/en/docs/about-claude/models
+            max_tokens=4096,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="claude-3-haiku-20240307",
+        )
+
+        return message.content
+
+    @property
+    def call_timeout(self):
+        # This is to stay within free tier for Gemini
+        return 5
+
+
+if __name__ == "__main__":
+    # to try this model, go to repo's root and run
+    # python -m models.claude
+    model = Claude()
+    output = model("Write Python code to sum two numbers.")
+    print(output)
