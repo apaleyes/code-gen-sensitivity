25861e329bb1c7e89f02c1c0720025eef71e91fe
Andrei Paleyes <apalees@gmail.com>
2025-05-09 23:54:54 +0100

    Few fixes to paraphraser

--- augmenters/paraphraser.py
+++ augmenters/paraphraser.py
@@ -3,13 +3,13 @@ from .base import BaseAugmenter
 class ParaphraserAugmenter(BaseAugmenter):
     def __init__(self, rate, paraphrases_file):
         super().__init__("Paraphraser", rate)
-        rate = 1 - rate
 
         if rate < 0.001:
             # special case, make sure we return unaltered text for 0
             # don't compare to 0.0 directly to avoid precision issues
             self.is_no_op = True
         else:
+            rate = 1 - rate
             self.range = self._determine_range(rate)
             self.paraphrases_df = self._load_paraphrases(paraphrases_file)
             self.is_no_op = False
@@ -45,5 +45,8 @@ class ParaphraserAugmenter(BaseAugmenter):
     
 
     def get_bleu_score(self, paraphrase):
+        if paraphrase in self.get_original_prompts():
+            return 0.0
+
         record = self.paraphrases_df[self.paraphrases_df['paraphrase'] == paraphrase].unique()
         return record['sacre_bleu']
